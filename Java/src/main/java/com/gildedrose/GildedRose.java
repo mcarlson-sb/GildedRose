00fd16c377e50552da628900ac4f95a6f15fe0de
Matthew Carlson <mcarlson.ucsb@gmail.com>
2019-02-19 00:43:47 -0800

    ! moved logic to improve cohesiveness

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,32 +15,15 @@ class GildedRose {
 
     private void updateItem(Item item) {
         if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-        } else if (item.name.equals("Aged Brie")) {
-            updateAgedBrie(item);
             return;
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            updateBackstagePass(item);
-            return;
-        } else {
-            updateDefaultItem(item);
         }
 
-        if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
+        if (item.name.equals("Aged Brie")) {
+            updateAgedBrie(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            updateBackstagePass(item);
+        } else {
+            updateDefaultItem(item);
         }
     }
 
@@ -49,6 +32,12 @@ class GildedRose {
             item.quality = item.quality - 1;
         }
         item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
+        }
     }
 
     private void updateBackstagePass(Item item) {
