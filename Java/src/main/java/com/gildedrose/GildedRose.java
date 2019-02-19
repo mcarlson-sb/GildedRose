a10d09b9c3b404498d37774e00bed152e2209343
Matthew Carlson <mcarlson.ucsb@gmail.com>
2019-02-19 00:38:10 -0800

    ! moved logic to improve cohesiveness

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -17,6 +17,7 @@ class GildedRose {
         if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
         } else if (item.name.equals("Aged Brie")) {
             updateAgedBrie(item);
+            return;
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             updateBackstagePass(item);
         } else {
@@ -73,6 +74,12 @@ class GildedRose {
             item.quality = item.quality + 1;
         }
         item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
     }
 
     @Override
