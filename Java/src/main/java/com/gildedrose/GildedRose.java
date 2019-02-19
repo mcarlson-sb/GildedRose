48ced2ddd9464bb5dac0680a09219cbf83e70410
Matthew Carlson <mcarlson.ucsb@gmail.com>
2019-02-19 00:45:44 -0800

    R use only less than to avoid bugs

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -28,13 +28,13 @@ class GildedRose {
     }
 
     private void updateDefaultItem(Item item) {
-        if (item.quality > 0) {
+        if (0 < item.quality) {
             item.quality = item.quality - 1;
         }
         item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
-            if (item.quality > 0) {
+            if (0 < item.quality) {
                 item.quality = item.quality - 1;
             }
         }
