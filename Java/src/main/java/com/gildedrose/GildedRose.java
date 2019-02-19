4d8532f40eb461dc20f6eaf1c85fcd910aa15664
Matthew Carlson <mcarlson.ucsb@gmail.com>
2019-02-19 00:46:43 -0800

    R simplify expression

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -59,7 +59,7 @@ class GildedRose {
         item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
-            item.quality = item.quality - item.quality;
+            item.quality = 0;
         }
     }
 
