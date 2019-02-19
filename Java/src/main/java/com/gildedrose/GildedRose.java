df7860e3bbc64d0ca474e35f1c99a2a040b07189
Matthew Carlson <mcarlson.ucsb@gmail.com>
2019-02-19 00:36:00 -0800

    ! move logic to individual strategies

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -23,10 +23,6 @@ class GildedRose {
             updateDefaultItem(item);
         }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
-
         if (item.sellIn < 0) {
             if (!item.name.equals("Aged Brie")) {
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -50,6 +46,7 @@ class GildedRose {
         if (item.quality > 0) {
             item.quality = item.quality - 1;
         }
+        item.sellIn = item.sellIn - 1;
     }
 
     private void updateBackstagePass(Item item) {
@@ -68,12 +65,14 @@ class GildedRose {
                 }
             }
         }
+        item.sellIn = item.sellIn - 1;
     }
 
     private void updateAgedBrie(Item item) {
         if (item.quality < 50) {
             item.quality = item.quality + 1;
         }
+        item.sellIn = item.sellIn - 1;
     }
 
     @Override
